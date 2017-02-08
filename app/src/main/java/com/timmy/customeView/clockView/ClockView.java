806ee57f1edaecbbf5d662f93accb5ede3434941
Timmy_zzh <timmy_zzh@163.com>
2017-02-08 10:04:56 +0800

    自定义钟表

--- app/src/main/java/com/timmy/customeView/clockView/ClockView.java
+++ app/src/main/java/com/timmy/customeView/clockView/ClockView.java
@@ -10,6 +10,7 @@ import android.util.TypedValue;
 import android.view.View;
 
 import com.timmy.R;
+import com.timmy.library.util.Logger;
 
 import java.util.Calendar;
 
@@ -23,6 +24,7 @@ import java.util.Calendar;
  * 2.复写onDraw（）方法
  */
 public class ClockView extends View {
+    private String TAG = "ClockView";
 
     //绘制表盘画笔
     private Paint clockPaint;
@@ -61,8 +63,6 @@ public class ClockView extends View {
 //        int color = typedArray.getColor(R.styleable.CustomImageView_imageType, 0);
 //        typedArray.recycle();
         //设置画笔参数和基本属性
-        //获取当前时间
-        calendar = Calendar.getInstance();
         //钟表的默认宽高
         mWidth = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 256f, getResources().getDisplayMetrics());
         mHeight = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 256f, getResources().getDisplayMetrics());
@@ -75,7 +75,6 @@ public class ClockView extends View {
         minuteWidth = 6;
         secondWidth = 4;
 
-
         //设置表盘画笔样式
         clockPaint = new Paint();
         //描边
@@ -85,7 +84,7 @@ public class ClockView extends View {
         //设置画笔颜色
         clockPaint.setColor(Color.BLUE);
         //设置画笔宽度
-        clockPaint.setStrokeWidth(6);
+        clockPaint.setStrokeWidth(10);
 
         //设置中心点的画笔样式
         centerPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -94,6 +93,7 @@ public class ClockView extends View {
 
         //设置小时文本的画笔样式
         numPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        numPaint.setTextSize(28);
         numPaint.setColor(Color.RED);
         numPaint.setStyle(Paint.Style.FILL);
         //文本对齐方式
@@ -118,9 +118,58 @@ public class ClockView extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
+
+        //获取当前时间
+        calendar = Calendar.getInstance();
+
+        //表盘中心点
+        int centerX = mWidth / 2;
+        int centerY = mHeight / 2;
+
         //绘制表盘-画圆  --参数信息(中心点,半径,画笔)
-        canvas.drawCircle(mWidth / 2, mHeight / 2, mWidth / 2, clockPaint);
+        int radius = centerX - 20;
+        canvas.drawCircle(centerX, centerY, radius, clockPaint);
+        //画表盘中心圆圈
+        canvas.drawCircle(centerX, centerY, 20, centerPaint);
 
+        /**
+         * 画小时的刻度和数字文本-使用for循环
+         * 和画板的save()和restore()方法结合处理
+         * 并且配合画板旋转
+         */
+        for (int i = 1; i <= 12; i++) {
+            canvas.save();
+            //画板旋转 --参数(选择角度,锚点)
+            canvas.rotate(30 * i, centerX, centerY);
+            //画小时刻度,一小段直线
+            canvas.drawLine(centerX, centerY - radius, centerX, centerY - radius + 16, clockPaint);
+            //画小时文本
+            canvas.drawText(i + "", centerX, centerY - radius + 40, numPaint);
+            canvas.restore();
+        }
 
+        //根据时间绘制时分秒
+        int hour = calendar.get(Calendar.HOUR);
+        int minute = calendar.get(Calendar.MINUTE);
+
+        canvas.save();
+        canvas.rotate(hour * 30 + 0.5f * minute, centerX, centerY);
+        canvas.drawLine(centerX, centerY - 160, centerX, centerY + 40, hourPaint);
+        canvas.restore();
+        //分
+//        minute = calendar.get(Calendar.MINUTE);
+        canvas.save();
+        canvas.rotate(minute * 6, centerX, centerY);
+        canvas.drawLine(centerX, centerY - 200, centerX, centerY + 50, minutePaint);
+        canvas.restore();
+
+        //秒
+        int second = calendar.get(Calendar.SECOND);
+        canvas.save();
+        canvas.rotate(second * 6, centerX, centerY);
+        canvas.drawLine(centerX, centerY - 260, centerX, centerY + 60, secondPaint);
+        canvas.restore();
+        Logger.d(TAG, "hour:" + hour + ",minute:" + minute + ",second" + second);
+        postInvalidateDelayed(1000);
     }
 }
